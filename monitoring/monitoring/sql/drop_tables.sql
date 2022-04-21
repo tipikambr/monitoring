0216a07b16978f77ade65161cc5f10c78ea71b15
tipikambr <tipikambr@yandex.ru>
2022-04-21 19:44:09 +0300

    if exists when drop table

--- monitoring/monitoring/sql/drop_tables.sql
+++ monitoring/monitoring/sql/drop_tables.sql
@@ -1,9 +1,9 @@
-drop table activity;
-drop table company;
-drop table project;
-drop table projects_users;
-drop table task;
-drop table tokens;
-drop table users;
-drop table geolocation;
-drop table firebase;
+drop table if exists activity ;
+drop table if exists company;
+drop table if exists project;
+drop table if exists projects_users;
+drop table if exists task;
+drop table if exists tokens;
+drop table if exists users;
+drop table if exists geolocation;
+drop table if exists firebase;
